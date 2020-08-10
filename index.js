070083ae8259747185c397f58ba25f5cd192f51b
tscotce <tatyana.celovsky@gmail.com>
2020-08-10 14:56:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+document.body.appendChild(element)
+let ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(ul)
+
+let element = document.querySelector("p#greeting");
+element.innerHTML = 'Hello, DOM!'
+
+let header = document.getElementById("div#header");
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+main.remove();
