307ff25b2aa4e1871d5bcec42db7167f0e672cd2
Tatyana Celovsky <tatyana.celovsky@gmail.com>
2020-08-14 15:27:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,4 @@ let header = document.getElementById("div#header");
 header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
 
 main.remove();
+body.removeChild(body.querySelector('main#main'));
